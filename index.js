003beb1eafd7b554c09705f38e63dd86cfce0400
Roman Mylenkyi <i.ua4@i.ua>
2016-11-05 11:34:21 +0000

    gh-pages

---
+++ index.js
@@ -0,0 +1,9 @@
+//alert('Hallo');
+//console.log('Hallo');
+//document.write('Hallo');
+
+
+var sum = 1980 + 10 + 12;
+var text = 'My magic number: ';
+var result = text + sum;
+document.write(sum);
